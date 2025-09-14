e755a4916efee1d7b725609e2b63271c8cbcea79
More Samarth Nagesh <115178193+moresamarth2020@users.noreply.github.com>
2025-09-14 21:17:47 +0530

    Add files via upload

---
+++ Day_77.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercise 10
+# - Use the NewsAPI and the requests module to fetch the daily news related to different topics. Go to: https://newsapi.org/ and explore the various options to build you application
+
+# In[ ]:
+
+
+
+
